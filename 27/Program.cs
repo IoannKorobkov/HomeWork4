8535a7469d0ac22de626eee0fea3f77ebed115d4
Ioann <korobkov-1@mail.ru>
2023-02-10 20:45:35 +0300

    number27

---
+++ 27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int x = Convert.ToInt32(Console.ReadLine());
+int SumNumbers(int a)
+{
+    int result = 0;
+    while (a > 0)
+    {
+        result = result + (a % 10);
+        a = a / 10;
+    }
+    return result;
+}
+Console.WriteLine(SumNumbers(x));
